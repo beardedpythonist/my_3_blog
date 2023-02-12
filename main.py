57316d0c6cd10ccbb464e0478369ade4f2d25df8
Alexander Gordey <alexnickgordey3@gmail.com>
2023-02-12 17:04:19 +0300

    уже с БД!

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
 import sqlite3
 
-
-Mercedec = ['mercedes','Mercedes', 'Merc', 'MERCEDES', 'MERC','мерседес', 'Мерседес','Мерс', 'МЕРСЕДЕС']
+# поиск по марке
+Mercedec = ['mercedes','Mercedes', 'Merc', 'мерс', 'MERCEDES', 'MERC','мерседес', 'Мерседес','Мерс', 'МЕРСЕДЕС']
 BMW = ['BMW','Bmw','bmw', 'БМВ','Бмв',',бмв']
 Land = ['Land Rover','LAND ROVER','Land','Ленд Ровер','Ленд','Ланд Ровер',]
 volvo =['Volvo', 'VOLVO','Вольво','ВОЛЬВО']
 audi =['Audi','AUDI','ауди','Ауди','АУДИ']
 vw =['Volkswagen''VOLKSWAGEN''VW''фольксваген''Фольксваген''ФОЛЬСКВАГЕН']
 ford = ['Ford','FORD','ford','форд','Форд','ФОРД']
-jag = ['Jaguar','JAGUAR','ягуар','Ягуар','ЯГУАР']
-
+Jaguar = ['Jaguar','JAGUAR','ягуар','Ягуар','ЯГУАР']
 poisk = input('введите марку авто: ')
-slov ={'merc':['mercedes','Mercedes', 'Merc', 'MERCEDES', 'MERC','мерседес', 'Мерседес','Мерс', 'МЕРСЕДЕС'],
-       'BMW': ['BMW','Bmw','bmw', 'БМВ','Бмв']}
-x = int
+slov = {'Mercedes':['mercedes','Mercedes', 'Merc', 'MERCEDES', 'мерс',
+                            'MERC','мерседес', 'Мерседес','Мерс', 'МЕРСЕДЕС'],
+        'BMW':['BMW','Bmw','bmw', 'БМВ','Бмв','бмв'],
+        'Land': ['Land Rover','LAND ROVER','Land','Ленд Ровер','Ленд','Ланд Ровер'],
+        'volvo': ['Volvo', 'VOLVO','Вольво','ВОЛЬВО'],
+        'audi' :['Audi','AUDI','ауди','Ауди','АУДИ'],
+        'vw' :['Volkswagen','VOLKSWAGEN','VW','фольксваген','Фольксваген','ФОЛЬСКВАГЕН'],
+        'ford' : ['Ford','FORD','ford','форд','Форд','ФОРД'],
+        'Jaguar' : ['Jaguar','JAGUAR','ягуар','Ягуар','ЯГУАР']}
+
+flag = False
 for key, val in slov.items():
     if poisk in val:
         print(key)
+        flag = True
+        break
+
+
+if not flag:
+    print('Машина не найдена')
+
 
 
 # with  sqlite3.connect('mydb.db') as mojabaza:
