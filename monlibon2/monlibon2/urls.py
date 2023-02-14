cdc40cd34e75635091cfe7abe628c0a4b7d9a6d1
Alexander Gordey <alexnickgordey3@gmail.com>
2023-02-14 16:36:42 +0300

    с шаблоном но недоделан

--- monlibon2/monlibon2/urls.py
+++ monlibon2/monlibon2/urls.py
@@ -1,12 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path
+from shop2.views import index, shop
 
-from shop2.views import index1, index2
+urlpatterns = [path('admin/', admin.site.urls),
+    path('', index, name='home'),
+    path('shop/',shop, name= 'shop')]
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index1),
-    path('list/', index2)
 
-]
--- monlibon2/shop2/views.py
+++ monlibon2/shop2/views.py
@@ -2,17 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
 
+def index(request):
+    return render(request, 'shop2/index.html')
 
-def index1(request):
-    return HttpResponse('Привет лунатикам!!')
-
-
-def index2(request):
-    bbb = Parts.objects.all()
-    return render(request, 'shop2/list.html', {'bbb': bbb})
-
-
-
-    pass
-
-# Create your views here.
+def shop(request):
+    return render(request, 'shop2/shop.html')
