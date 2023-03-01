dbce484ba16e70857004585573d0cd9dbcc2deb6
Alexander Gordey <alexnickgordey3@gmail.com>
2023-03-01 16:00:46 +0300

    с новым шаблоном

--- monlibon2/shop2/admin.py
+++ monlibon2/shop2/admin.py
@@ -4,6 +4,10 @@ class PartsAdmin(admin.ModelAdmin) :
     list_display = ('name', 'artikul', 'price', 'rubric', 'car_name')
     list_display_links = ('name', 'price')
     search_fields = ('name', 'price')
+
+
+
+
 admin.site.register(Parts, PartsAdmin)
 admin.site.register(Rubric)
 admin.site.register(Car_name)
